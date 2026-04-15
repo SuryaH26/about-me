99b0033cd67b84bb832de5c1c8e81e9942b5438e
Surya H <68045855+SuryaH26@users.noreply.github.com>
2026-04-15 10:50:00 +0000

    the code

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # Load the file as a document 
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
 
 loader = PyPDFLoader("Resume -Surya.pdf")
 documents = loader.load()
 
 # Make the document into chunks 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
     chunk_overlap=50
@@ -13,16 +18,13 @@ splitter = RecursiveCharacterTextSplitter(
 chunks = splitter.split_documents(documents)
 
 # Convert the doc into embeddings using OpenAIEmbeddings 
-from langchain.embeddings import OpenAIEmbeddings
 embeddings = OpenAIEmbeddings()
 
 # Store the embeddings into a FAISS vector store for retrieval 
-from langchain.vectorstores import FAISS
+
 vectorstore = FAISS.from_documents(chunks, embeddings)
 
 # RAG Pipeline 
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
 
 retriever = vectorstore.as_retriever()
 
@@ -30,7 +32,6 @@ qa = RetrievalQA.from_chain_type(
     llm=ChatOpenAI(),
     retriever=retriever
 )
-
 
 # test query 
 query = "What are Surya's skills ?"
@@ -40,10 +41,8 @@ print(response)
 
 
 # UI 
-# import streamlit as st
-
-# st.title("My Resume Chatbot")
-# query = st.text_input("Ask something about me:")
-# if query:
-#     response = qa.run(query)
-#     st.write(response)
+st.title("My Resume Chatbot")
+query = st.text_input("Ask something about me:")
+if query:
+    response = qa.run(query)
+    st.write(response)
